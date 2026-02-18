3c299b15e6e9db009b4677aee22dd14be1646980
Vaibhav Bhaladhare <108606392+Vaibhav1744@users.noreply.github.com>
2026-02-18 19:22:40 +0530

    Refactor EmailService to use Map for email data

--- Backend/src/main/java/com/app/portfolio/service/EmailService.java
+++ Backend/src/main/java/com/app/portfolio/service/EmailService.java
@@ -1,11 +1,11 @@
 package com.app.portfolio.service;
-import jakarta.mail.internet.MimeMessage;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.mail.javamail.JavaMailSender;
-import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
@@ -16,29 +16,27 @@ public class EmailService {
 
     private final WebClient webClient = WebClient.builder()
             .baseUrl("https://api.resend.com")
-            .defaultHeader("Authorization", "Bearer " + apiKey)
-            .defaultHeader("Content-Type", "application/json")
             .build();
 
     public void sendContactEmail(String name, String email, String message) {
 
-        String json = """
-        {
-          "from": "Portfolio <onboarding@resend.dev>",
-          "to": ["vaibhav.unitydev@gmail.com"],
-          "subject": "New Portfolio Contact from %s",
-          "html": "<h2>New Message</h2><p><b>Name:</b> %s</p><p><b>Email:</b> %s</p><p>%s</p>"
-        }
-        """.formatted(name, name, email, message);
-
         webClient.post()
                 .uri("/emails")
-                .bodyValue(json)
+                .header("Authorization", "Bearer " + apiKey)
+                .header("Content-Type", "application/json")
+                .bodyValue(Map.of(
+                        "from", "onboarding@resend.dev",
+                        "to", "vaibhav.unitydev@gmail.com",
+                        "subject", "🚀 New Portfolio Contact from " + name,
+                        "html",
+                        "<h2>New Contact Message</h2>" +
+                                "<p><strong>Name:</strong> " + name + "</p>" +
+                                "<p><strong>Email:</strong> " + email + "</p>" +
+                                "<p><strong>Message:</strong></p>" +
+                                "<p>" + message + "</p>"
+                ))
                 .retrieve()
                 .bodyToMono(String.class)
                 .block();
     }
-
-    System.out.println("API KEY VALUE: " + apiKey);
-
 }
